4314aa30c6432ba9313a104966d55f28f24c1562
recillah khamala <recillahk@gmail.com>
2023-02-15 17:46:04 +0300

    Create the registration and login page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-
-    
-  # associations
-  has_many :foods, dependent: :destroy
-  has_many :recipes, dependent: :destroy
-  
-  # validations
-  validates :name, presence: true, uniqueness: true
 end
