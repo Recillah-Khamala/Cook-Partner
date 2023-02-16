7fa22cf77d114df338d46bb07f2a9c69bae9094d
recillah khamala <recillahk@gmail.com>
2023-02-17 01:24:54 +0300

    generate recipe controller file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'recipes/index'
+  get 'recipes/show'
+  get 'recipes/new'
+  get 'recipes/create'
+  get 'recipes/edit'
+  get 'recipes/update'
+  get 'recipes/destroy'
   devise_for :users
 
   # root 'foods#show'
